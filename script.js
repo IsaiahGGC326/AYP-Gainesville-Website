e894edf3250118e87c47045038c3a534342fffb6
IsaiahGGC326 <ijimenez2@ggc.edu>
2025-03-27 22:06:39 -0400

    Added Videos to Carousel

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function greetUser() {
     alert("Hello! Welcome to the AYP Gainesville Youth website");
 }
+
+// Stops the videos from playing when moving to another slide //
+document.addEventListener("DOMContentLoaded", function() {
+    let carousel = document.getElementById('carouselExample');
+
+    carousel.addEventListener('slid.bs.carousel', function() {
+        let iframes = carousel.querySelectorAll("iframe");
+        iframes.forEach(iframe => {
+            let src = iframe.src;
+            iframe.src = src; // Reloads the iframe to stop the video
+        });
+    });
+});
